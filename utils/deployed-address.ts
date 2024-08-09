8b953b5901debafda7fd849c5f8f420ff767659d
lokieverboss <134183121+lokieverboss@users.noreply.github.com>
2024-08-09 16:46:23 +0800

    Add files via upload

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x704bD855FFefb18B7587B6D03CcB37Fa59C336D1'
+export const ProxyAdmin = '0xFfE3036BB7AC5Fc4dde9656A83740ab0c6703230'
+export const SWTRImplementation = '0x2d15FB1f6Ba8E7f73dC3e3bb7f4ef1779c9127eF'
+    
